d22de8cbce763e0b3cceb46f74b750e42a06fd5e
Bryan Ashley <bryan@weebly.com>
2017-09-29 15:17:35 -0400

    Test circle ci again

--- tests/Integration/BelongsToManyMutatedTest.php
+++ tests/Integration/BelongsToManyMutatedTest.php
@@ -28,7 +28,7 @@ class BelongsToManyMutatedTest extends TestCase
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'circle-test'),
-            'username' => env('DB_USERNAME', 'ubuntu'),
+            'username' => env('DB_USERNAME', 'mutate'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
